cc8cd04961eff1f243780d0b375e49249925b2ff
Mahin5580 <mmkmahin123@gmail.com>
2024-09-25 03:25:47 +0600

    some fixes done

--- js/utilities.js
+++ js/utilities.js
@@ -13,6 +13,6 @@ function getTextFieldValueById(id){
 // To blog page
 
 document.getElementById('btn-nav-blog').addEventListener('click',function(){
-    window.location.href='http://127.0.0.1:5500/blog.html'
+    window.location.href='blog.html'
 })
 
